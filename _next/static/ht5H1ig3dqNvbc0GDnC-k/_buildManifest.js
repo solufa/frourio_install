9e079ecb0cd9681cb5d4df0e5b859de49385f577
Zono-san <Zono-san@users.noreply.github.com>
2021-07-23 08:07:25 +0000

    deploy: a008b4a47aceb351f670c4e49a1e41ed3bd6db82

--- _next/static/ht5H1ig3dqNvbc0GDnC-k/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/414-efde2f963362c9d952ef.js","static/css/c193e51de719b0f82960.css","static/chunks/pages/index-dce265a2afc96364e96b.js"],"/_error":["static/chunks/pages/_error-a9f53acb468cbab8a6cb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
