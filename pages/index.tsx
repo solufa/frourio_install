0ed11154c2b7c681479d601666472ecd815b34ac
Zono-san <87744612+Zono-san@users.noreply.github.com>
2021-08-02 17:59:04 +0900

    onClick

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { apiClient } from '~/utils/apiClient'
 import UserBanner from '~/components/UserBanner'
 import type { Task } from '$prisma/client'
 import type { FormEvent, ChangeEvent } from 'react'
+import { getAllJSDocTagsOfKind } from 'typescript'
 
 const Home = () => {
   const { data: tasks, error, revalidate } = useAspidaSWR(apiClient.tasks)
@@ -37,6 +38,10 @@ const Home = () => {
     revalidate()
   }, [])
 
+  const onClick = useCallback(() => {
+    alert("クリックされました")
+  }, [])
+
   if (error) return <div>failed to load</div>
   if (!tasks) return <div>loading...</div>
   
@@ -48,96 +53,86 @@ const Home = () => {
       </Head>
 
       <main className={styles.main}>
-        <div className="wrapper">
+        <div className="wrapper" onClick={onClick}>
           <div className={styles.square}>
             <div className={styles.squareIn}>
-              <div className={styles.box1}></div>
-              <div className={styles.box2}></div>
-              <div className={styles.box3}></div>
-              <div className={styles.box4}></div>
-              <div className={styles.box5}></div>
-              <div className={styles.box6}></div>
-              <div className={styles.box7}></div>
-              <div className={styles.box8}></div>
-              <div className={styles.box9}></div>
-              <div className={styles.box10}></div>
-              <div className={styles.box11}></div>
-              <div className={styles.box12}></div>
-              <div className={styles.box13}></div>
-              <div className={styles.box14}></div>
-              <div className={styles.box15}></div>
-              <div className={styles.box16}></div>
-              <div className={styles.box17}></div>
-              <div className={styles.box18}></div>
-              <div className={styles.box19}></div>
-              <div className={styles.box20}></div>
-              <div className={styles.box21}></div>
-              <div className={styles.box22}></div>
-              <div className={styles.box23}></div>
-              <div className={styles.box24}></div>
-              <div className={styles.box25}></div>
-              <div className={styles.box26}></div>
-              <div className={styles.box27}></div>
-              <div className={styles.box28}>
-                <div className={styles.stone1}></div>
-              </div>
-              <div className={styles.box29}>
-                <div className={styles.stone2}></div>
-              </div>
-              <div className={styles.box30}></div>
-              <div className={styles.box31}></div>
-              <div className={styles.box32}></div>
-              <div className={styles.box33}></div>
-              <div className={styles.box34}></div>
-              <div className={styles.box35}></div>
-              <div className={styles.box36}>
-                <div className={styles.stone4}></div>
-              </div>
-              <div className={styles.box37}>
-                <div className={styles.stone3}></div>
-              </div>
-              <div className={styles.box38}></div>
-              <div className={styles.box39}></div>
-              <div className={styles.box40}></div>
-              <div className={styles.box41}></div>
-              <div className={styles.box42}></div>
-              <div className={styles.box43}></div>
-              <div className={styles.box44}></div>
-              <div className={styles.box45}></div>
-              <div className={styles.box46}></div>
-              <div className={styles.box47}></div>
-              <div className={styles.box48}></div>
-              <div className={styles.box49}></div>
-              <div className={styles.box50}></div>
-              <div className={styles.box51}></div>
-              <div className={styles.box52}></div>
-              <div className={styles.box53}></div>
-              <div className={styles.box54}></div>
-              <div className={styles.box55}></div>
-              <div className={styles.box56}></div>
-              <div className={styles.box57}></div>
-              <div className={styles.box58}></div>
-              <div className={styles.box59}></div>
-              <div className={styles.box60}></div>
-              <div className={styles.box61}></div>
-              <div className={styles.box62}></div>
-              <div className={styles.box63}></div>
-              <div className={styles.box64}></div>
+                <div className={styles.box1}></div>
+                <div className={styles.box2}></div>
+                <div className={styles.box3}></div>
+                <div className={styles.box4}></div>
+                <div className={styles.box5}></div>
+                <div className={styles.box6}></div>
+                <div className={styles.box7}></div>
+                <div className={styles.box8}></div>
+                <div className={styles.box9}></div>
+                <div className={styles.box10}></div>
+                <div className={styles.box11}></div>
+                <div className={styles.box12}></div>
+                <div className={styles.box13}></div>
+                <div className={styles.box14}></div>
+                <div className={styles.box15}></div>
+                <div className={styles.box16}></div>
+                <div className={styles.box17}></div>
+                <div className={styles.box18}></div>
+                <div className={styles.box19}></div>
+                <div className={styles.box20}></div>
+                <div className={styles.box21}></div>
+                <div className={styles.box22}></div>
+                <div className={styles.box23}></div>
+                <div className={styles.box24}></div>
+                <div className={styles.box25}></div>
+                <div className={styles.box26}></div>
+                <div className={styles.box27}></div>
+                <div className={styles.box28}>
+                  <div className={styles.stone1}></div>
+                </div>
+                <div className={styles.box29}>
+                  <div className={styles.stone2}></div>
+                </div>
+                <div className={styles.box30}></div>
+                <div className={styles.box31}></div>
+                <div className={styles.box32}></div>
+                <div className={styles.box33}></div>
+                <div className={styles.box34}></div>
+                <div className={styles.box35}></div>
+                <div className={styles.box36}>
+                  <div className={styles.stone4}></div>
+                </div>
+                <div className={styles.box37}>
+                  <div className={styles.stone3}></div>
+                </div>
+                <div className={styles.box38}></div>
+                <div className={styles.box39}></div>
+                <div className={styles.box40}></div>
+                <div className={styles.box41}></div>
+                <div className={styles.box42}></div>
+                <div className={styles.box43}></div>
+                <div className={styles.box44}></div>
+                <div className={styles.box45}></div>
+                <div className={styles.box46}></div>
+                <div className={styles.box47}></div>
+                <div className={styles.box48}></div>
+                <div className={styles.box49}></div>
+                <div className={styles.box50}></div>
+                <div className={styles.box51}></div>
+                <div className={styles.box52}></div>
+                <div className={styles.box53}></div>
+                <div className={styles.box54}></div>
+                <div className={styles.box55}></div>
+                <div className={styles.box56}></div>
+                <div className={styles.box57}></div>
+                <div className={styles.box58}></div>
+                <div className={styles.box59}></div>
+                <div className={styles.box60}></div>
+                <div className={styles.box61}></div>
+                <div className={styles.box62}></div>
+                <div className={styles.box63}></div>
+                <div className={styles.box64}></div>
             </div>
           </div>
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
