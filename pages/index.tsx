84c6271cd06f38465dd252154827fc34bd4af8d4
Zono-san <87744612+Zono-san@users.noreply.github.com>
2021-08-02 18:42:12 +0900

    black&white Stone

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,6 +42,18 @@ const Home = () => {
     alert("クリックされました")
   }, [])
 
+  const clickWhiteStone = useCallback(() => {
+    alert("白い石がクリックされました")
+  }, [])
+
+  const clickBlackStone = useCallback(() => {
+    alert("黒い石がクリックされました")
+  }, [])
+
+  const noStone = useCallback(() => {
+    alert("ここに石はありません")
+  }, [])
+
   if (error) return <div>failed to load</div>
   if (!tasks) return <div>loading...</div>
   
@@ -56,78 +68,78 @@ const Home = () => {
         <div className="wrapper" onClick={onClick}>
           <div className={styles.square}>
             <div className={styles.squareIn}>
-                <div className={styles.box1}></div>
-                <div className={styles.box2}></div>
-                <div className={styles.box3}></div>
-                <div className={styles.box4}></div>
-                <div className={styles.box5}></div>
-                <div className={styles.box6}></div>
-                <div className={styles.box7}></div>
-                <div className={styles.box8}></div>
-                <div className={styles.box9}></div>
-                <div className={styles.box10}></div>
-                <div className={styles.box11}></div>
-                <div className={styles.box12}></div>
-                <div className={styles.box13}></div>
-                <div className={styles.box14}></div>
-                <div className={styles.box15}></div>
-                <div className={styles.box16}></div>
-                <div className={styles.box17}></div>
-                <div className={styles.box18}></div>
-                <div className={styles.box19}></div>
-                <div className={styles.box20}></div>
-                <div className={styles.box21}></div>
-                <div className={styles.box22}></div>
-                <div className={styles.box23}></div>
-                <div className={styles.box24}></div>
-                <div className={styles.box25}></div>
-                <div className={styles.box26}></div>
-                <div className={styles.box27}></div>
+                <div className={styles.box1} onClick={noStone}></div>
+                <div className={styles.box2} onClick={noStone}></div>
+                <div className={styles.box3} onClick={noStone}></div>
+                <div className={styles.box4} onClick={noStone}></div>
+                <div className={styles.box5} onClick={noStone}></div>
+                <div className={styles.box6} onClick={noStone}></div>
+                <div className={styles.box7} onClick={noStone}></div>
+                <div className={styles.box8} onClick={noStone}></div>
+                <div className={styles.box9} onClick={noStone}></div>
+                <div className={styles.box10} onClick={noStone}></div>
+                <div className={styles.box11} onClick={noStone}></div>
+                <div className={styles.box12} onClick={noStone}></div>
+                <div className={styles.box13} onClick={noStone}></div>
+                <div className={styles.box14} onClick={noStone}></div>
+                <div className={styles.box15} onClick={noStone}></div>
+                <div className={styles.box16} onClick={noStone}></div>
+                <div className={styles.box17} onClick={noStone}></div>
+                <div className={styles.box18} onClick={noStone}></div>
+                <div className={styles.box19} onClick={noStone}></div>
+                <div className={styles.box20} onClick={noStone}></div>
+                <div className={styles.box21} onClick={noStone}></div>
+                <div className={styles.box22} onClick={noStone}></div>
+                <div className={styles.box23} onClick={noStone}></div>
+                <div className={styles.box24} onClick={noStone}></div>
+                <div className={styles.box25} onClick={noStone}></div>
+                <div className={styles.box26} onClick={noStone}></div>
+                <div className={styles.box27} onClick={noStone}></div>
                 <div className={styles.box28}>
-                  <div className={styles.stone1}></div>
+                  <div className={styles.stone1} onClick={clickBlackStone}></div>
                 </div>
                 <div className={styles.box29}>
-                  <div className={styles.stone2}></div>
+                  <div className={styles.stone2} onClick={clickWhiteStone}></div>
                 </div>
-                <div className={styles.box30}></div>
-                <div className={styles.box31}></div>
-                <div className={styles.box32}></div>
-                <div className={styles.box33}></div>
-                <div className={styles.box34}></div>
-                <div className={styles.box35}></div>
+                <div className={styles.box30} onClick={noStone}></div>
+                <div className={styles.box31} onClick={noStone}></div>
+                <div className={styles.box32} onClick={noStone}></div>
+                <div className={styles.box33} onClick={noStone}></div>
+                <div className={styles.box34} onClick={noStone}></div>
+                <div className={styles.box35} onClick={noStone}></div>
                 <div className={styles.box36}>
-                  <div className={styles.stone4}></div>
+                  <div className={styles.stone4} onClick={clickWhiteStone}></div>
                 </div>
                 <div className={styles.box37}>
-                  <div className={styles.stone3}></div>
+                  <div className={styles.stone3} onClick={clickBlackStone}></div>
                 </div>
-                <div className={styles.box38}></div>
-                <div className={styles.box39}></div>
-                <div className={styles.box40}></div>
-                <div className={styles.box41}></div>
-                <div className={styles.box42}></div>
-                <div className={styles.box43}></div>
-                <div className={styles.box44}></div>
-                <div className={styles.box45}></div>
-                <div className={styles.box46}></div>
-                <div className={styles.box47}></div>
-                <div className={styles.box48}></div>
-                <div className={styles.box49}></div>
-                <div className={styles.box50}></div>
-                <div className={styles.box51}></div>
-                <div className={styles.box52}></div>
-                <div className={styles.box53}></div>
-                <div className={styles.box54}></div>
-                <div className={styles.box55}></div>
-                <div className={styles.box56}></div>
-                <div className={styles.box57}></div>
-                <div className={styles.box58}></div>
-                <div className={styles.box59}></div>
-                <div className={styles.box60}></div>
-                <div className={styles.box61}></div>
-                <div className={styles.box62}></div>
-                <div className={styles.box63}></div>
-                <div className={styles.box64}></div>
+                <div className={styles.box38} onClick={noStone}></div>
+                <div className={styles.box39} onClick={noStone}></div>
+                <div className={styles.box40} onClick={noStone}></div>
+                <div className={styles.box41} onClick={noStone}></div>
+                <div className={styles.box42} onClick={noStone}></div>
+                <div className={styles.box43} onClick={noStone}></div>
+                <div className={styles.box44} onClick={noStone}></div>
+                <div className={styles.box45} onClick={noStone}></div>
+                <div className={styles.box46} onClick={noStone}></div>
+                <div className={styles.box47} onClick={noStone}></div>
+                <div className={styles.box48} onClick={noStone}></div>
+                <div className={styles.box49} onClick={noStone}></div>
+                <div className={styles.box50} onClick={noStone}></div>
+                <div className={styles.box51} onClick={noStone}></div>
+                <div className={styles.box52} onClick={noStone}></div>
+                <div className={styles.box53} onClick={noStone}></div>
+                <div className={styles.box54} onClick={noStone}></div>
+                <div className={styles.box55} onClick={noStone}></div>
+                <div className={styles.box56} onClick={noStone}></div>
+                <div className={styles.box57} onClick={noStone}></div>
+                <div className={styles.box58} onClick={noStone}></div>
+                <div className={styles.box59} onClick={noStone}></div>
+                <div className={styles.box60} onClick={noStone}></div>
+                <div className={styles.box61} onClick={noStone}></div>
+                <div className={styles.box62} onClick={noStone}></div>
+                <div className={styles.box63} onClick={noStone}></div>
+                <div className={styles.box64} onClick={noStone}></div>
             </div>
           </div>
         </div>
